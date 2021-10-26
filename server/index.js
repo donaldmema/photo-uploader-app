74ec43902f57b077cbfb5ad9e55161f9199aea51
aziznal <aziznal.dev@gmail.com>
2021-10-26 18:11:07 +0300

    setup server to handle incoming image uploads

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const fs = require("fs");
+const express = require("express");
+const { StatusCodes } = require("http-status-codes");
+
+const app = express();
+const port = 3000;
+
+app.post("/upload", (req, res) => {
+  let realFile = Buffer.from(req.body.image, "base64");
+
+  fs.writeFileSync(req.body.name, realFile, "utf8");
+
+  res
+    .status(StatusCodes.CREATED)
+    .send({ message: "Image successfully saved on server" });
+});
+
+app.listen(port, () => console.log("Started on PORT 3000"));
